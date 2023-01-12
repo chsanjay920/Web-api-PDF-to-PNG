db939b18f0978bf88ba34fe2d0b939cdc7d04bff
chintala sanjay <61927341+chsanjay920@users.noreply.github.com>
2023-01-12 09:26:22 +0530

    1st commit
    api designed ( working perfectly )

---
+++ apiForPdfToPng/apiForPdfToPng/Controllers/PdfToPngController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace apiForPdfToPng.Controllers
+{
+    [Route("api/[controller]/[Name]")]
+    [ApiController]
+    public class PdfToPngController : ControllerBase
+    {
+        // GET: api/<PdfToPngController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult ToPng(IFormFile file)
+        {
+            byte[] pdfByteArray = null;
+            try
+            {
+                if (file != null)
+                {
+                    using var memoryStream = new MemoryStream();
+                    file!.CopyToAsync(memoryStream);
+                    pdfByteArray = memoryStream.ToArray();
+                }
+                var imagesList = Freeware.Pdf2Png.ConvertAllPages(pdfByteArray);
+                //foreach (var img in imagesList)
+                //{
+                //    using (Image image = Image.FromStream(new MemoryStream(img)))
+                //    {
+                //        image.Save("output.png", ImageFormat.Png);
+                //    }
+                //}
+                return File(imagesList[0], "image/jpng");
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return Ok();
+        }
+
+
+    }
+}
